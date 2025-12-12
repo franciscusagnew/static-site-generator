a1cde0ef06a94b772c4445423f8d5cfce0fd20f9
Franciscus Agnew <agnew_f@hotmail.com>
2025-12-12 10:38:02 -0500

    Modified inline_markdown.py and test_inline_markdown.py

--- src/test_inlinemarkdown.py
+++ src/test_inlinemarkdown.py
@@ -1,6 +1,11 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+  split_nodes_delimiter,
+  extract_markdown_images,
+  extract_markdown_links
+)
 from textnode import TextNode, TextType
+
 
 class TestInlineMarkdown(unittest.TestCase):
   def test_delim_bold(self):
@@ -63,9 +68,9 @@ class TestInlineMarkdown(unittest.TestCase):
     new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
     self.assertEqual(
       [
-          TextNode("bold", TextType.BOLD),
-          TextNode(" and ", TextType.TEXT),
-          TextNode("italic", TextType.ITALIC),
+        TextNode("bold", TextType.BOLD),
+        TextNode(" and ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
       ],
       new_nodes,
     )
@@ -75,11 +80,29 @@ class TestInlineMarkdown(unittest.TestCase):
     new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
     self.assertListEqual(
       [
-          TextNode("This is text with a ", TextType.TEXT),
-          TextNode("code block", TextType.CODE),
-          TextNode(" word", TextType.TEXT),
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("code block", TextType.CODE),
+        TextNode(" word", TextType.TEXT),
       ],
       new_nodes,
+    )
+
+  def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+      "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+  def test_extract_markdown_links(self):
+    matches = extract_markdown_links(
+      "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+    )
+    self.assertListEqual(
+      [
+        ("link", "https://boot.dev"),
+        ("another link", "https://blog.boot.dev"),
+      ],
+      matches,
     )
 
 if __name__ == "__main__":
